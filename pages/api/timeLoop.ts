5eea52a6d697f2aa22f31df29d0bd2c714f076c1
Kalebe <kaka.leebe@gmail.com>
2024-07-11 20:58:59 -0300

    update timeLoop.ts

--- pages/api/timeLoop.ts
+++ pages/api/timeLoop.ts
@@ -5,12 +5,11 @@ let booleanState = true; // Estado inicial do booleano
 export default function timeLoop(req: NextApiRequest, res: NextApiResponse) {
     if (booleanState) {
         res.status(200).json('Hello World! The boolean is true');
-        res.send(true);
     } else {
         res.status(200).json('Hello World! The boolean is false');
-        res.send(false);
     }
 
     // Alternar o estado do booleano para o próximo ciclo
     booleanState = !booleanState;
+    res.send(booleanState);
 }
