6a589e7791d957b5ab36c1a3e3bb56489602672b
Kalebe <kaka.leebe@gmail.com>
2024-07-11 20:44:01 -0300

    update timeLoop.ts

--- pages/api/timeLoop.ts
+++ pages/api/timeLoop.ts
@@ -1,6 +1,14 @@
 import { NextApiRequest, NextApiResponse } from "next";
 
 export default function timeLoop(req: NextApiRequest, res: NextApiResponse) {
-    res.status(200).json({ text: 'Hello' });
-    console.log(req.body);
+    let number = 1
+    if (number == 1) {
+        res.status(200).json('Hello World! \n The number is 1 (true)');
+        res.send(true);
+        number = 2;
+    }  else {
+        res.status(200).json('Hello World! \n The number is 2 (false)');
+        res.send(false);
+        number = 1;
+    }
 }
