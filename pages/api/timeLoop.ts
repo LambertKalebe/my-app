30e1c87ac57e4a4e24de5f17319c318e9b8a40c3
Kalebe <kaka.leebe@gmail.com>
2024-07-11 20:31:50 -0300

    modified:   pages/api/timeLoop.ts

--- pages/api/timeLoop.ts
+++ pages/api/timeLoop.ts
@@ -1,4 +1,6 @@
-export default function timeLoop(req, res) {
+import { NextApiRequest, NextApiResponse } from "next";
+
+export default function timeLoop(req: NextApiRequest, res: NextApiResponse) {
     res.status(200).json({ text: 'Hello' });
     console.log(req.body);
-  }
+}
