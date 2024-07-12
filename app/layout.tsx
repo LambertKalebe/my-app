af0fe665b18fd6f8167ed6e6acec88215ad02356
Kalebe <kaka.leebe@gmail.com>
2024-07-12 17:49:09 -0300

    modified:   app/layout.tsx 	modified:   package-lock.json 	modified:   package.json

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,7 @@
 import './globals.css'
 import { Inter } from 'next/font/google'
-
+import { SpeedInsights } from "@vercel/speed-insights/next"
+import { Analytics } from "@vercel/analytics/react"
 const inter = Inter({ subsets: ['latin'] })
 
 export const metadata = {
@@ -16,6 +17,8 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body className={inter.className}>
+      <Analytics/>
+      <SpeedInsights/>
         {children}
       </body>
     </html>
