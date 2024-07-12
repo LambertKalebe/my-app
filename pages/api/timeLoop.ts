ef55bb7760e855ed0a5e491e55b5fc4fd0eb0501
Kalebe <kaka.leebe@gmail.com>
2024-07-11 21:39:14 -0300

    update timeLoop.ts

--- pages/api/timeLoop.ts
+++ pages/api/timeLoop.ts
@@ -3,6 +3,8 @@ import { NextApiRequest, NextApiResponse } from "next";
 let booleanState = true; // Estado inicial do booleano
 
 export default function handler(req: NextApiRequest, res: NextApiResponse) {
+    console.log('Headers:', req.headers); // Verifique os headers recebidos
+    
     if (req.method === 'GET') {
         // Endpoint público para obter o estado atual do booleano
         res.status(200).json({ booleanState });
