274480b5d20f171a1381df52d085325fa2e26001
Kalebe <kaka.leebe@gmail.com>
2024-07-11 21:44:45 -0300

    update timeLoop.ts

--- pages/api/timeLoop.ts
+++ pages/api/timeLoop.ts
@@ -8,7 +8,7 @@ export default function handler(req: NextApiRequest, res: NextApiResponse) {
     if (req.method === 'GET') {
         // Endpoint público para obter o estado atual do booleano
         res.status(200).json({ booleanState });
-    } else if (req.method === 'POST' && req.headers.cron === 'true') {
+    } else if (req.method === 'POST' && req.headers.cron == 'true') {
         // Verifica se o header 'cron' tem o valor 'true' para permitir a atualização
         booleanState = !booleanState; // Alternar o estado do booleano
         res.status(200).json({ message: 'Boolean state updated successfully.' });
