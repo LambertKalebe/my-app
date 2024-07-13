9fb64c593eeae8cb467e4184271cd10e3a2262d8
Kalebe <kaka.leebe@gmail.com>
2024-07-12 22:29:40 -0300

    update 7 files, delete 6 files and create 1 file

--- components/component/navbar.tsx
+++ components/component/navbar.tsx
@@ -19,7 +19,7 @@ export function Navbar() {
 
   return (
     <header className="bg-background border-b px-4 lg:px-6 h-14 flex items-center justify-between">
-      <Link href="#" className="flex items-center gap-2" prefetch={false}>
+      <Link href="/" className="flex items-center gap-2" prefetch={false}>
         <Image
           src="/lionIcon.svg"
           width={50}
